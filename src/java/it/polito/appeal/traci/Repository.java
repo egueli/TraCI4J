9a985a389eaeed19659931e3815990fc164b1ce7
Pietro Niccoli <piemail@tiscali.it>
2012-01-10 13:08:48 +0000

    pulito codice

--- src/java/it/polito/appeal/traci/Repository.java
+++ src/java/it/polito/appeal/traci/Repository.java
@@ -27,7 +27,6 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
